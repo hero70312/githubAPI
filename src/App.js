4e1126f8840818544fc40a1aa4ae490bc6534208
Darren <hero70312@gmail.com>
2019-10-01 22:35:35 +0800

    remove useless code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import ImageAvatar from './Components/imageAvatar';
-
 import './App.css';
 
 
